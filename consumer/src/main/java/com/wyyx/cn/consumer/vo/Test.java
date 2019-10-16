15a9f089886e50dc4c2994eb76d4b3798bbc11d0
youjief <youjiefeng1@gmail.com>
2019-10-16 15:52:13 +0800

    sa

---
+++ consumer/src/main/java/com/wyyx/cn/consumer/vo/Test.java
@@ -0,0 +1,12 @@
+package com.wyyx.cn.consumer.vo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 91917
+ * Date: 2019/10/16
+ * Time: 15:50
+ * Description: No Description
+ */
+public class Test {
+    int i=1;
+}
