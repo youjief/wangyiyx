4832c22722c4f6ca4bb078b2f90c2475bd48a38e
youjief <youjiefeng1@gmail.com>
2019-10-17 14:04:00 +0800

    mapper

---
+++ provider/src/main/java/com/wyyx/cn/provider/model/Order.java
@@ -0,0 +1,136 @@
+package com.wyyx.cn.provider.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Order {
+    private Integer orderId;
+
+    private Integer userId;
+
+    private Integer goodsId;
+
+    private String deliveryAddr;
+
+    private String goodsName;
+
+    private String goodsCount;
+
+    private BigDecimal goodsPrice;
+
+    private String orderChannel;
+
+    private String orderStatus;
+
+    private Date createTime;
+
+    private Date payTime;
+
+    private String goodsFreight;
+
+    private String goodsScores;
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(Integer goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getDeliveryAddr() {
+        return deliveryAddr;
+    }
+
+    public void setDeliveryAddr(String deliveryAddr) {
+        this.deliveryAddr = deliveryAddr;
+    }
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    public String getGoodsCount() {
+        return goodsCount;
+    }
+
+    public void setGoodsCount(String goodsCount) {
+        this.goodsCount = goodsCount;
+    }
+
+    public BigDecimal getGoodsPrice() {
+        return goodsPrice;
+    }
+
+    public void setGoodsPrice(BigDecimal goodsPrice) {
+        this.goodsPrice = goodsPrice;
+    }
+
+    public String getOrderChannel() {
+        return orderChannel;
+    }
+
+    public void setOrderChannel(String orderChannel) {
+        this.orderChannel = orderChannel;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getPayTime() {
+        return payTime;
+    }
+
+    public void setPayTime(Date payTime) {
+        this.payTime = payTime;
+    }
+
+    public String getGoodsFreight() {
+        return goodsFreight;
+    }
+
+    public void setGoodsFreight(String goodsFreight) {
+        this.goodsFreight = goodsFreight;
+    }
+
+    public String getGoodsScores() {
+        return goodsScores;
+    }
+
+    public void setGoodsScores(String goodsScores) {
+        this.goodsScores = goodsScores;
+    }
+}
